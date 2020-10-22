0e63c5d7bcc44d491be6c14d1ab712205f66c413
Marc Catrisse <marc@catrisse.net>
2020-10-22 23:24:24 +0200

    trabajando...

--- homework1/homework_1.py
+++ homework1/homework_1.py
@@ -1,15 +1,40 @@
 
-from scipy import special
+import math
+from scipy.stats import binom
 
 def calculate_individual_prob_saturated(n):
     # P(E) = 1 - P(E^C) = 1-(1-1/n)^n
     return 1-pow(1-(1/n),n)
 
+def aCb(n, k):
+    return math.factorial(n) / (math.factorial(k)*math.factorial(n-k))
+
+def prob_x_bins_empty(N):
+    res = 0
+    for n in range(1,N):
+        if n % 2 == 0:
+            res = res - aCb(N, n)*pow((N-n)/N,N)
+        else:
+            res = res + aCb(N, n)*pow((N-n)/N,N)
+        print(1-res,n)
+
+def prob_x_bins_empty(N):
+    res = 0
+    for n in range(1,N):
+        if n % 2 == 0:
+            res = res - aCb(N, n)*pow((N-n)/N,N)
+        else:
+            res = res + aCb(N, n)*pow((N-n)/N,N)
+        print(1-res,n)
 
 
+def k_occupied_input_lines(N):
+    p = calculate_individual_prob_saturated(N)
+    print(p)
+    k = 22
+    print(aCb(N,k)*pow(p,k)*pow(1-p,N-k))
 
-
-
+    
 
 
 
@@ -22,4 +47,4 @@ def main():
 
 
 
-main()
+k_occupied_input_lines(32)
