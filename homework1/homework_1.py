b5fbc28e2e0e29123e3bc3db888789620f6d99df
Marc Catrisse <marc@catrisse.net>
2020-10-24 18:34:15 +0200

    working... falta i=k (preguntar profe)

--- homework1/homework_1.py
+++ homework1/homework_1.py
@@ -1,10 +1,23 @@
 
 import math
 from scipy.stats import binom
+import matplotlib.pyplot as plt
 
-def calculate_individual_prob_saturated(n):
+def calculate_individual_E_saturated(n):
     # P(E) = 1 - P(E^C) = 1-(1-1/n)^n
     return 1-pow(1-(1/n),n)
+
+def calculate_individual_E_non_saturated(N, prob):
+    # Total probability formula
+    sum = 0
+    for n in range(0,N+1):
+        p1 = 1-pow(1-(1/N),n)
+        p2 = binom.pmf(n,N,prob)
+        sum += sum + (p1*p2)
+    #print("N", N, "prob", prob, "sum", sum)
+    return sum
+
+    
 
 def aCb(n, k):
     return math.factorial(n) / (math.factorial(k)*math.factorial(n-k))
@@ -34,7 +47,51 @@ def k_occupied_input_lines(N):
     k = 22
     print(aCb(N,k)*pow(p,k)*pow(1-p,N-k))
 
+def plot_non_saturated_1():
+    f1 = plt.figure()
+    f2 = plt.figure()
+    ax1 = f1.add_subplot()
+    ax1.set(xlabel='N', ylabel='E(x)', title='Non-Saturated switch single expected value')
+    ax2 = f2.add_subplot()
+    ax2.set(xlabel='N', ylabel='sum(N, E(x))', title='Non-Saturated switch aggregated expected value')
+    data = []
+    dataAgregatted = []
+    iList = []
+    for i in range(1,128+1):
+        e = calculate_individual_E_non_saturated(i, 1/2)
+        data.append(e)
+        dataAgregatted.append(e*i)
+        iList.append(i)
+        print("i-> ",i,"Expected aggregated", e*i, "Expected single", e)
+    ax1.plot(iList,data)
+    f1.savefig("non-saturated-part1.png")
+    ax2.plot(iList, dataAgregatted)
+    f2.savefig("non-saturated-part1-aggre.png")
+    plt.show()
+
     
+def plot_saturated_1():
+    f1 = plt.figure()
+    f2 = plt.figure()
+    ax1 = f1.add_subplot()
+    ax1.set(xlabel='N', ylabel='E(x)', title='Saturated switch single expected value')
+    ax2 = f2.add_subplot()
+    ax2.set(xlabel='N', ylabel='sum(N, E(x))', title='Saturated switch aggregated expected value')
+    data = []
+    dataAgregatted = []
+    iList = []
+    for i in range(1,128+1):
+        e = calculate_individual_E_saturated(i)
+        data.append(e)
+        dataAgregatted.append(e*i)
+        iList.append(i)
+        print("i-> ",i,"Expected aggregated", e*i, "Expected single", e)
+    ax1.plot(iList,data)
+    f1.savefig("saturated-part1.png")
+    ax2.plot(iList, dataAgregatted)
+    f2.savefig("saturated-part1-aggre.png")
+    plt.show()
+
 
 
 
@@ -46,5 +103,5 @@ def main():
         print("i-> ",i,"Expected aggregated", prob*i, "Expected single", prob)
 
 
-
-k_occupied_input_lines(32)
+plot_saturated_1()
+plot_non_saturated_1()
