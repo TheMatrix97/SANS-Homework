748cf32d28b61e65ca7fa8ddcf662cc46b9456bc
Marc Catrisse <marc@catrisse.net>
2020-10-28 20:07:22 +0100

    add part 2 non saturated switch

--- homework1/homework_1.py
+++ homework1/homework_1.py
@@ -3,6 +3,9 @@ import math
 from scipy.stats import binom
 import matplotlib.pyplot as plt
 
+def aCb(n, k):
+    return math.factorial(n) / (math.factorial(k)*math.factorial(n-k))
+
 def calculate_individual_E_saturated(n):
     # P(E) = 1 - P(E^C) = 1-(1-1/n)^n
     return 1-pow(1-(1/n),n)
@@ -14,14 +17,9 @@ def calculate_individual_E_non_saturated(N, prob):
     for n in range(0,N+1):
         p1 = 1-pow(1-(1/N),n)
         p2 = binom.pmf(n,N,prob)
-        sum += sum + (p1*p2)
+        sum = sum + (p1*p2)
     #print("N", N, "prob", prob, "sum", sum)
     return sum
-
-    
-
-def aCb(n, k):
-    return math.factorial(n) / (math.factorial(k)*math.factorial(n-k))
 
 def prob_x_bins_empty(N):
     res = 0
@@ -32,6 +30,37 @@ def prob_x_bins_empty(N):
             res = res + aCb(N, n)*pow((N-n)/N,N)
         print(res,n)
 
+def calculate_prob_at_least_one_bin_occupied(k, n, m): #N output M input
+    res = 0
+    for r in range(1, k+1):
+        #print(aux)
+        if r % 2 == 0:
+            res = res - aCb(k, r)* pow(1-r/n, m)
+        else:
+            res = res + aCb(k, r)* pow(1-r/n, m)
+    return res
+
+def plot_non_saturated_2_k(): #N = M
+    N = [4,8,32,64]
+    prob = 1/2
+    for n in N:
+        f1 = plt.figure()
+        ax1 = f1.add_subplot()
+        ax1.set(xlabel='k', ylabel='p(sum(x) = k)', title='N = '+ str(n) +' - Non Saturated switch')
+        res = []
+        for k in range(0, n+1):
+            if k == 0:
+                res.append(binom.pmf(k,n,prob)) #Probability input lines are empty
+                print(res)
+            else:
+                res_total_prob = 0;
+                for m in range(k, n+1): #total Prob formula for each possible m input
+                    aux = 1-calculate_prob_at_least_one_bin_occupied(k, n, m)
+                    res_total_prob = res_total_prob + (aux * binom.pmf(m,n,prob)) #num of success m (inputs) in n input lines
+                res.append(res_total_prob)
+        ax1.plot(list(range(0,n+1)),res)
+        #f1.show()
+        f1.savefig(str(n)+"_non_saturated_part2.png")
 
 def plot_saturated_2_k():
     N = [4,8,32,64,128]
@@ -45,23 +74,6 @@ def plot_saturated_2_k():
         ax1.plot(list(range(1,n+1)),res)
         #f1.show()
         f1.savefig(str(n)+"_saturated_part2.png")
-
-def calculate_prob_at_least_one_bin_occupied(k, n, m): #N output M input
-    res = 0
-    for r in range(1, k+1):
-        #print(aux)
-        if r % 2 == 0:
-            res = res - aCb(k, r)* pow(1-r/n, m)
-        else:
-            res = res + aCb(k, r)* pow(1-r/n, m)
-    return res
-
-
-def k_occupied_input_lines(N):
-    p = calculate_individual_prob_saturated(N)
-    print(p)
-    k = 22
-    print(aCb(N,k)*pow(p,k)*pow(1-p,N-k))
 
 def plot_non_saturated_1():
     f1 = plt.figure()
@@ -123,4 +135,5 @@ def main():
 #plot_non_saturated_1()
 #prob_x_bins_empty(32)
 
-plot_saturated_2_k()
+#plot_saturated_2_k()
+plot_non_saturated_2_k()
