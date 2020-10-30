8a9690f2d5aad752dc53b6c5feb289410ddda80a
Marc Catrisse <marc@catrisse.net>
2020-10-30 17:13:21 +0100

    finished part 3

--- homework1/homework_1.py
+++ homework1/homework_1.py
@@ -32,13 +32,9 @@ def prob_x_bins_empty(N):
 
 def calculate_prob_at_least_one_bin_occupied(k, n, m): #N output M input
     res = 0
-    for r in range(1, k+1):
-        #print(aux)
-        if r % 2 == 0:
-            res = res - aCb(k, r)* pow(1-r/n, m)
-        else:
-            res = res + aCb(k, r)* pow(1-r/n, m)
-    return res
+    for r in range(0, k+1):
+        res = res + (pow(-1,r) * aCb(k, r) * pow((k-r)/n,m))
+    return aCb(n,k) * res
 
 def plot_non_saturated_2_k(): #N = M
     N = [4,8,32,64]
@@ -55,7 +51,7 @@ def plot_non_saturated_2_k(): #N = M
             else:
                 res_total_prob = 0
                 for m in range(k, n+1): #total Prob formula for each possible m input
-                    aux = 1-calculate_prob_at_least_one_bin_occupied(k, n, m)
+                    aux = calculate_prob_at_least_one_bin_occupied(k, n, m)
                     res_total_prob = res_total_prob + (aux * binom.pmf(m,n,prob)) #num of success m (inputs) in n input lines
                 res.append(res_total_prob)
         ax1.plot(list(range(0,n+1)),res)
@@ -70,10 +66,46 @@ def plot_saturated_2_k():
         ax1.set(xlabel='k', ylabel='p(sum(x) = k)', title='N = '+ str(n) +' - Saturated switch')
         res = []
         for k in range(1, n+1):
-            res.append(1-calculate_prob_at_least_one_bin_occupied(k, n, n))
+            res.append(calculate_prob_at_least_one_bin_occupied(k, n, n))
         ax1.plot(list(range(1,n+1)),res)
         #f1.show()
         f1.savefig(str(n)+"_saturated_part2.png")
+
+def plot_saturated_3(): #if independent it will behaviour as binomial distribution
+    N = [4,8,32,64,128]
+    for n in N:
+        f1 = plt.figure()
+        ax1 = f1.add_subplot()
+        ax1.set(xlabel='k', ylabel='p(sum(x) = k)', title='N = '+ str(n) +' - Saturated switch - Xn and Xm independent')
+        res = []
+        for k in range(1, n+1):
+            prob = aCb(n,k) * binom.pmf(k,n,1/n) #k sucess in n outputs, where prob is 1/n
+            res.append(prob)
+        ax1.plot(list(range(1,n+1)),res)
+        #f1.show()
+        f1.savefig(str(n)+"_saturated_part3.png")
+
+def plot_non_saturated_3(): #N = M
+    N = [4,8,32,64]
+    prob = 1/2
+    for n in N:
+        f1 = plt.figure()
+        ax1 = f1.add_subplot()
+        ax1.set(xlabel='k', ylabel='p(sum(x) = k)', title='N = '+ str(n) +' - Non Saturated switch - Xn and Xm independent')
+        res = []
+        for k in range(0, n+1):
+            if k == 0:
+                res.append(binom.pmf(k,n,prob)) #Probability input lines are empty
+                print(res)
+            else:
+                res_total_prob = 0
+                for m in range(k, n+1): #total Prob formula for each possible m input
+                    aux = aCb(n,k) * binom.pmf(k,n,1/n) #Prob getting k success in n outputs
+                    res_total_prob = res_total_prob + (aux * binom.pmf(m,n,prob)) #num of success m (inputs) in n input lines
+                res.append(res_total_prob)
+        ax1.plot(list(range(0,n+1)),res)
+        #f1.show()
+        f1.savefig(str(n)+"_non_saturated_part3.png")
 
 def plot_non_saturated_1():
     f1 = plt.figure()
@@ -95,7 +127,7 @@ def plot_non_saturated_1():
     f1.savefig("non-saturated-part1.png")
     ax2.plot(iList, dataAgregatted)
     f2.savefig("non-saturated-part1-aggre.png")
-    plt.show()
+    #plt.show()
 
     
 def plot_saturated_1():
@@ -118,7 +150,7 @@ def plot_saturated_1():
     f1.savefig("saturated-part1.png")
     ax2.plot(iList, dataAgregatted)
     f2.savefig("saturated-part1-aggre.png")
-    plt.show()
+    #plt.show()
 
 
 
@@ -131,9 +163,9 @@ def main():
         print("i-> ",i,"Expected aggregated", prob*i, "Expected single", prob)
 
 
-#plot_saturated_1()
-#plot_non_saturated_1()
-#prob_x_bins_empty(32)
-
-#plot_saturated_2_k()
+plot_saturated_1()
+plot_non_saturated_1()
+plot_saturated_2_k()
 plot_non_saturated_2_k()
+plot_non_saturated_3()
+plot_saturated_3()
