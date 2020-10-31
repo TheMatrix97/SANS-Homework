e6df7b74a4197f10f3c11dd94e8a080c885e56fc
Marc Catrisse <marc@catrisse.net>
2020-10-31 21:52:20 +0100

    cleaning

--- homework1/homework_1.py
+++ homework1/homework_1.py
@@ -28,6 +28,29 @@ def calculate_prob_at_least_one_bin_occupied(k, n, m): #N output M input
         res = res + (pow(-1,r) * aCb(k, r) * pow((k-r)/n,m))
     return aCb(n,k) * res
 
+def plot_non_saturated_3(): #N = M
+    N = [4,8,32,64]
+    prob = 1/2
+    for n in N:
+        f1 = plt.figure()
+        ax1 = f1.add_subplot()
+        ax1.set(xlabel='k', ylabel='p(sum(x) = k)', title='N = '+ str(n) +' - Non Saturated switch - Xn and Xm independent')
+        res = []
+        for k in range(0, n+1):
+            if k == 0:
+                res.append(binom.pmf(k,n,prob)) #Probability input lines are empty
+                print(res)
+            else:
+                res_total_prob = 0
+                for m in range(k, n+1): #total Prob formula for each possible m input
+                    aux = aCb(n,k) * binom.pmf(k,n,1/n) #Prob getting k success in n outputs
+                    res_total_prob = res_total_prob + (aux * binom.pmf(m,n,prob)) #num of success m (inputs) in n input lines
+                res.append(res_total_prob)
+        ax1.plot(list(range(0,n+1)),res)
+        #f1.show()
+        f1.savefig(str(n)+"_non_saturated_part3.png")
+
+
 def plot_non_saturated_2_k(): #N = M
     N = [4,8,32,64]
     prob = 1/2
@@ -50,54 +73,6 @@ def plot_non_saturated_2_k(): #N = M
         #f1.show()
         f1.savefig(str(n)+"_non_saturated_part2.png")
 
-def plot_saturated_2_k():
-    N = [4,8,32,64,128]
-    for n in N:
-        f1 = plt.figure()
-        ax1 = f1.add_subplot()
-        ax1.set(xlabel='k', ylabel='p(sum(x) = k)', title='N = '+ str(n) +' - Saturated switch')
-        res = []
-        for k in range(1, n+1):
-            res.append(calculate_prob_at_least_one_bin_occupied(k, n, n))
-        ax1.plot(list(range(1,n+1)),res)
-        #f1.show()
-        f1.savefig(str(n)+"_saturated_part2.png")
-
-def plot_saturated_3(): #if independent it will behaviour as binomial distribution
-    N = [4,8,32,64,128]
-    for n in N:
-        f1 = plt.figure()
-        ax1 = f1.add_subplot()
-        ax1.set(xlabel='k', ylabel='p(sum(x) = k)', title='N = '+ str(n) +' - Saturated switch - Xn and Xm independent')
-        res = []
-        for k in range(1, n+1):
-            prob = aCb(n,k) * binom.pmf(k,n,1/n) #k sucess in n outputs, where prob is 1/n
-            res.append(prob)
-        ax1.plot(list(range(1,n+1)),res)
-        #f1.show()
-        f1.savefig(str(n)+"_saturated_part3.png")
-
-def plot_non_saturated_3(): #N = M
-    N = [4,8,32,64]
-    prob = 1/2
-    for n in N:
-        f1 = plt.figure()
-        ax1 = f1.add_subplot()
-        ax1.set(xlabel='k', ylabel='p(sum(x) = k)', title='N = '+ str(n) +' - Non Saturated switch - Xn and Xm independent')
-        res = []
-        for k in range(0, n+1):
-            if k == 0:
-                res.append(binom.pmf(k,n,prob)) #Probability input lines are empty
-                print(res)
-            else:
-                res_total_prob = 0
-                for m in range(k, n+1): #total Prob formula for each possible m input
-                    aux = aCb(n,k) * binom.pmf(k,n,1/n) #Prob getting k success in n outputs
-                    res_total_prob = res_total_prob + (aux * binom.pmf(m,n,prob)) #num of success m (inputs) in n input lines
-                res.append(res_total_prob)
-        ax1.plot(list(range(0,n+1)),res)
-        #f1.show()
-        f1.savefig(str(n)+"_non_saturated_part3.png")
 
 def plot_non_saturated_1():
     f1 = plt.figure()
@@ -121,6 +96,35 @@ def plot_non_saturated_1():
     f2.savefig("non-saturated-part1-aggre.png")
     #plt.show()
 
+
+def plot_saturated_3(): #if independent it will behaviour as binomial distribution
+    N = [4,8,32,64,128]
+    for n in N:
+        f1 = plt.figure()
+        ax1 = f1.add_subplot()
+        ax1.set(xlabel='k', ylabel='p(sum(x) = k)', title='N = '+ str(n) +' - Saturated switch - Xn and Xm independent')
+        res = []
+        for k in range(1, n+1):
+            prob = aCb(n,k) * binom.pmf(k,n,1/n) #k sucess in n outputs, where prob is 1/n
+            res.append(prob)
+        ax1.plot(list(range(1,n+1)),res)
+        #f1.show()
+        f1.savefig(str(n)+"_saturated_part3.png")
+
+
+def plot_saturated_2_k():
+    N = [4,8,32,64,128]
+    for n in N:
+        f1 = plt.figure()
+        ax1 = f1.add_subplot()
+        ax1.set(xlabel='k', ylabel='p(sum(x) = k)', title='N = '+ str(n) +' - Saturated switch')
+        res = []
+        for k in range(1, n+1):
+            res.append(calculate_prob_at_least_one_bin_occupied(k, n, n))
+        ax1.plot(list(range(1,n+1)),res)
+        #f1.show()
+        f1.savefig(str(n)+"_saturated_part2.png")
+
     
 def plot_saturated_1():
     f1 = plt.figure()
@@ -143,8 +147,6 @@ def plot_saturated_1():
     ax2.plot(iList, dataAgregatted)
     f2.savefig("saturated-part1-aggre.png")
     #plt.show()
-
-
 
 
 
